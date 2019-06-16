3a9937eac869108bfbe28338165e03da492bf195
Dan Covill <daniel.covill@gmail.com>
2019-06-16 10:30:01 -0600

    Add 48px icon needed by FF Add basic visual elements to popup

---
+++ app/popup.js
@@ -0,0 +1,12 @@
+window.browser = (function () {
+  return window.msBrowser ||
+    window.browser ||
+    window.chrome;
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+	//get the current tab's page
+	//check the page against the blacklist
+	
+	//update visual elements on the popup to show/hide items and update text
+});
