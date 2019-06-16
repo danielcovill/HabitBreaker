9483814e91a667a15771d6e23652b63eace099fc
Dan Covill <daniel.covill@gmail.com>
2019-06-15 22:27:22 -0600

    Add placeholders for options page and popup with necessary manifest changes Change main page to use a form and add fields for reason/timeout

--- app/app.js
+++ app/app.js
@@ -4,10 +4,22 @@ window.browser = (function () {
     window.chrome;
 })();
 
+// Clicking the proceed button asks the background process to keep going
 document.getElementById("proceed").addEventListener("click", function() {
+	let timeout = document.getElementById("timeout").value;
 	browser.runtime.sendMessage({
-		type: "triggerRedirect"
+		type: "triggerRedirect",
+		timeoutMins: timeout
 	});
+});
+
+document.getElementById("redirectForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+	let reason = e.srcElement.reason.value;
+	let timeout = e.srcElement.timeout.value;
+	
+	//TODO: log the reason, set the timeout
+	debugger;
 });
 
 document.addEventListener("DOMContentLoaded", function () {
