57aac4f48f53778c49ba5da035f5c8a3e132d256
Dan Covill <daniel.covill@gmail.com>
2019-09-19 08:53:59 -0600

    Minor reference fixes to chrome codepath

--- app/background.js
+++ app/background.js
@@ -13,30 +13,55 @@ function checkURL(requestDetails) {
 	} else if (!!window.chrome) {
 		//can't use promises here because chrome's webRequest.onBeforeRequest can't handle them
 		if(blacklist.isCurrentlyBlacklisted(url)) {
-			return { redirectUrl: browser.extension.getURL(`app/index.html?redirect=${encodeURIComponent(requestDetails.url)}`) };
+			return { redirectUrl: chrome.extension.getURL(`app/index.html?redirect=${encodeURIComponent(requestDetails.url)}`) };
 		}
 	} else {
 		throw "Unsupported browser";
 	}
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-	checkURL,
-	{ urls: ["<all_urls>"], types: ["main_frame"] },
-	["blocking"]
-);
+if (!!window.browser) {
+	browser.webRequest.onBeforeRequest.addListener(
+		checkURL,
+		{ urls: ["<all_urls>"], types: ["main_frame"] },
+		["blocking"]
+	);
 
-browser.runtime.onMessage.addListener(function (request, sender) {
-	switch (request.type) {
-		case "createRedirectException":
-			return blacklist.getMatchingBlacklistEntry(request.url).then((matchingBlacklistEntry) => {
-				if(!!matchingBlacklistEntry) {
-					return blacklist.addException(matchingBlacklistEntry.url, request.exceptionReason, (request.timeoutMins * 60));
-				} else {
-					throw `No matching exception entry for ${request.url}`;
-				}
-			});
-		default:
-			throw "Unexpected message to background";
-	}
-});
+	browser.runtime.onMessage.addListener(function (request, sender) {
+		switch (request.type) {
+			case "createRedirectException":
+				return blacklist.getMatchingBlacklistEntry(request.url).then((matchingBlacklistEntry) => {
+					if(!!matchingBlacklistEntry) {
+						return blacklist.addException(matchingBlacklistEntry.url, request.exceptionReason, (request.timeoutMins * 60));
+					} else {
+						throw `No matching exception entry for ${request.url}`;
+					}
+				});
+			default:
+				throw "Unexpected message to background";
+		}
+	});
+} else if (!!window.chrome) {
+	chrome.webRequest.onBeforeRequest.addListener(
+		checkURL,
+		{ urls: ["<all_urls>"], types: ["main_frame"] },
+		["blocking"]
+	);
+
+	chrome.runtime.onMessage.addListener(function (request, sender) {
+		switch (request.type) {
+			case "createRedirectException":
+				return blacklist.getMatchingBlacklistEntry(request.url).then((matchingBlacklistEntry) => {
+					if(!!matchingBlacklistEntry) {
+						return blacklist.addException(matchingBlacklistEntry.url, request.exceptionReason, (request.timeoutMins * 60));
+					} else {
+						throw `No matching exception entry for ${request.url}`;
+					}
+				});
+			default:
+				throw "Unexpected message to background";
+		}
+	});	
+} else {
+	throw "Unsupported browser";
+}
